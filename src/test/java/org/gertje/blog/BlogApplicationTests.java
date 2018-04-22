590858281e1f0c77eced49eafbf421870dab8edf
Bert Jan Schrijver <bertjan@revolution.nl>
2018-04-22 08:50:12 +0200

    Add rest-assured example

--- src/test/java/org/gertje/blog/BlogApplicationTests.java
+++ src/test/java/org/gertje/blog/BlogApplicationTests.java
@@ -1,16 +1,21 @@
 package org.gertje.blog;
 
+import org.apache.http.HttpStatus;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static io.restassured.RestAssured.when;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment=RANDOM_PORT)
 public class BlogApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void getNonExistingPostReturnsNotFound() {
+		when().get("/post/1").then().statusCode(HttpStatus.SC_NOT_FOUND);
 	}
 
 }
