b0824bb6d755a6ffdfc021dc1f85314b7f4bc9a2
Geert Mulders <g.c.w.m.mulders@gmail.com>
2018-04-22 14:35:18 +0200

    Fix RestAssured test

--- src/test/java/org/gertje/blog/BlogApplicationTests.java
+++ src/test/java/org/gertje/blog/BlogApplicationTests.java
@@ -1,8 +1,11 @@
 package org.gertje.blog;
 
+import io.restassured.RestAssured;
 import org.apache.http.HttpStatus;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,6 +15,14 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=RANDOM_PORT)
 public class BlogApplicationTests {
+
+	@LocalServerPort
+	int port;
+
+	@Before
+	public void init() {
+		RestAssured.port = port;
+	}
 
 	@Test
 	public void getNonExistingPostReturnsNotFound() {
