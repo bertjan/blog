a1ebf898cab8bb3e13b599c0166b898770fff14a
Bert Jan Schrijver <bertjan@revolution.nl>
2018-04-22 08:49:36 +0200

    Remove @EnableWebMvc (auto-configured by Spring Boot), fix repository test

--- src/main/java/org/gertje/blog/BlogApplication.java
+++ src/main/java/org/gertje/blog/BlogApplication.java
@@ -2,10 +2,8 @@ package org.gertje.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class BlogApplication {
 
 	public static void main(String[] args) {
